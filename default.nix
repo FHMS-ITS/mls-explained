a43f0a6c5ec82db91c325d30f7574a75b2a1a514
Sebastian Staffa <Sebastian_Staffa@web.de>
2020-05-27 21:13:54 +0200

    Changes for GitHub, i18n

--- default.nix
+++ default.nix
@@ -11,6 +11,4 @@ let
     libMLS = import ./libMLS/default.nix {inherit nixpkgs;};
     infrastructure = import ./infrastructure/default.nix {inherit nixpkgs libMLS;};
 
-    #integration_test = import ./integration-tests.nix;
-
 in pkgs.linkFarm "mls" [ {name="infrastructure"; path=infrastructure;} {name="libMLS";path=libMLS;} ]
